6fae8dced928ba4c3a60655ae62f07f8a4fdd7a2
nadimahmed1 <nadimarafat26@gmail.com>
2022-12-02 23:26:53 +0600

    react bootstrap banner added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Experts from '../../Experts/Experts';
+import Banner from '../Banner/Banner';
 import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div>
             <h1>Home Calling!!</h1>
+            <Banner></Banner>
             <Services></Services>
             <Experts></Experts>
         </div>
